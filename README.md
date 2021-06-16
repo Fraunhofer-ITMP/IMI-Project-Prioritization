c38c7504d2fba2342f033d201e04ca56d1794ad3
Yojana Gadiya <yojanagadiya@gmail.com>
2021-06-16 11:59:47 +0200

    Add basic file

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # IMI_Scrapper
+
+Repository having the code for scraping IMI projects for WP-1 FAIRPlus. The aim is to create extensive list of all IMI projects for prioritization purposes.
---
+++ src/constants.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+
+"""Scraping related constant"""
+
+DATA_DIR = "../data"
+# TODO: Replace these paths to abs paths in your computer (examples are given)
+chrome_driver_path = "C:/Users/Yojana.Gadiya/Downloads/chromedriver"
+

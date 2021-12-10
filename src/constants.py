cddb3217730d0b323b181a2d02b90af99f6bf4a0
vassilios ioannidis <vassilios.ioannidis@caprica.ch>
2021-12-10 14:38:29 +0100

    added more requirements and chromedriver instructions

--- src/constants.py
+++ src/constants.py
@@ -5,5 +5,5 @@
 
 DATA_DIR = "../data"
 # TODO: Replace these paths to abs paths in your computer (examples are given)
-chrome_driver_path = "C:/Users/Yojana.Gadiya/Downloads/chromedriver"
+chrome_driver_path = "chromedriver"
 

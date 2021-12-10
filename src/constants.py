42059e97e16c7d469406c44718d073128a999fc8
Yojana Gadiya <yojanagadiya@gmail.com>
2021-12-10 16:06:45 +0100

    Add docstring

--- src/constants.py
+++ src/constants.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 
-
 """Scraping related constant"""
 
 DATA_DIR = "../data"
-# TODO: Replace these paths to abs paths in your computer (examples are given)
+# TODO: Replace these paths to abs paths in your computer
 chrome_driver_path = "chromedriver"
-
